be96b3151a9458fc32b9f7baa3d4dfbfd28d0189
Steven Gangl <steven.h.gangl@gmail.com>
2023-02-09 13:37:50 -0600

    all done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,11 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item1){
+ basket.push(item1)
+    return true;
+}
+console.log(addItem(1));
+console.log(basket)
