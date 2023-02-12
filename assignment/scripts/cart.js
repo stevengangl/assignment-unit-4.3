aa501bc5a47a1bf255ec127fe13cd33288dd071b
Steven Gangl <steven.h.gangl@gmail.com>
2023-02-12 11:10:04 -0600

    2nd question done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,9 +3,20 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 let basket = [];
 
-function addItem(item1){
- basket.push(item1)
+function addItem(item1) {
+    basket.push(item1)
     return true;
 }
 console.log(addItem(1));
-console.log(basket)
+console.log(addItem(2));
+console.log(addItem(3));
+
+console.log(basket);
+
+function listItems() {
+    for (i = 0; i < basket.length; i++) {
+        console.log(basket[i]);
+
+    }
+}
+listItems();
